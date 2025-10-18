677a56adc0290ac8c2bb5903c3e4b240383897fe
Hayrapetyan Zhirayr <zhsayrapetyan@yandex.ru>
2025-10-18 16:47:13 +0000

    upd: configs

--- scripts/generate/generate_aggregated.sh
+++ scripts/generate/generate_aggregated.sh
@@ -1,3 +1,3 @@
 python3 -m graphgen.generate \
 --config_file graphgen/configs/aggregated_config.yaml \
---output_dir /workspace/hayrapetyan/GG_data/Qwen2.5-32B-IT-CFA
+--output_dir /workspace/hayrapetyan/GG_data/v1-Qwen2.5-32B-IT-CFA
